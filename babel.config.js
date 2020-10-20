1a0b69305b9651e6deaec5bc2c2e9ce72b5e4fc7
Ahmed Ayachi <AhmedAyachi@users.noreply.github.com>
2020-10-20 16:17:25 +0100

    add components and assets alias imports

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
 
-module.exports=(api)=>{
+module.exports=function(api){
     api.cache(true);
     return {
         presets:["babel-preset-expo"],
+        plugins:[
+            [
+                "module-resolver",
+                {
+                    alias:{
+                        "components":"./src/Components",
+                        "assets":"./src/Assets",
+                    },
+                },
+            ],
+        ],
     };
 };
