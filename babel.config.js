232167dc7f24f41fa3df03d6109d087f12fe18e2
AhmedAyachi <aayachi032@gmail.com>
2021-02-13 14:06:19 +0100

    A

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,6 @@ module.exports=function(api){
                 alias:{
                     "components":"./src/Components",
                     "assets":"./src/Assets",
-                    "afile":"",
                 },
             }],
         ],
