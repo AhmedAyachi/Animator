a85128153d34ce389fbe1213acbd8db962d859eb
AhmedAyachi <aayachi032@gmail.com>
2021-02-14 16:07:19 +0100

    to remove border style from App component

--- src/App.style.js
+++ src/App.style.js
@@ -1,10 +1,8 @@
 import {StyleSheet} from "react-native";
-import {border} from "afile";
 
 
 const css=StyleSheet.create({
     container:{
-        ...border(2,"solid","red"),
         flex:1,
         backgroundColor:"#fff",
         alignItems:"center",
