22560f701436986109645663221bd2f8f8d66daf
AhmedAyachi <aayachi032@gmail.com>
2021-03-13 11:54:28 +0100

    A

--- src/Components/BandView/BandProber/SongSection/SongView/Hooks.js
+++ src/Components/BandView/BandProber/SongSection/SongView/Hooks.js
@@ -5,55 +5,59 @@ import {fadeIn} from "afile";
 
 export const usePlayingAnimation=(duration)=>{
     const [playing,setPlaying]=useState(false);
-    const [isPaused,setIsPaused]=useState(!playing);
     const state=useRef({
         duration:duration*10,
-        //isPaused:!playing,
+        isPaused:!playing,
+        animation:null,
         width:Object.assign(new Animated.Value(0),{
             from:0,
             to:1,
         }),
     }).current;
-    const animation=Animated.timing(state.width,{
-        toValue:state.width.to,
-        duration:state.duration,
-        easing:Easing.linear,
-        useNativeDriver:false,
-    });
     return [
         state.width,
         playing,
         {
-            ...animation,
             start:()=>{
                 if(!playing){
-                    setPlaying(true);
-                    state.isPaused=false;
-                    alert("touched");
-                    animation.start(({finished})=>{
+                    state.animation=Animated.timing(state.width,{
+                        toValue:state.width.to,
+                        duration:state.duration,
+                        easing:Easing.linear,
+                        useNativeDriver:false,
+                    });
+                    state.animation.start(({finished})=>{
                         if(finished){
                             const {width}=state;
                             width.setValue(width.from);
+                            state.duration=duration*10;
                             setPlaying(false);
                         }
                     });
+                    state.isPaused=false;
+                    setPlaying(true);
                 }
             },
             toggle:function(){
                 state.isPaused=!state.isPaused;
-                //console.log(state.isPaused);
                 if(state.isPaused){
                     state.width.stopAnimation(value=>{
                         const toValue=state.width.to;
                         state.duration=state.duration*(toValue-value)/toValue;
-                        alert(state.duration);
                     });
                 }
                 else{
-                    animation.start(({finished})=>{
+                    state.animation=Animated.timing(state.width,{
+                        toValue:state.width.to,
+                        duration:state.duration,
+                        easing:Easing.linear,
+                        useNativeDriver:false,
+                    });
+                    state.animation.start(({finished})=>{
                         if(finished){
                             const {width}=state;
                             width.setValue(width.from);
+                            state.duration=duration*10;
                             setPlaying(false);
                         }
                     });
